063d710d9a1537a6b52dc92cc451326a2c68fd10
Omar <omarfayed381@gmail.com>
2023-08-10 19:05:18 -0700

    screw betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
- * main is the starting point
- * this function prints a tex
- * returns 0 when everything is correct
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\"");
 	return (0);
 }
